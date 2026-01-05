9a45b9a38b71a2d5d305379e15649412e9266662
Rushil Dasari <rsdasari01@gmail.com>
2026-01-05 10:09:33 -0500

    replace 'assets' with 'HVI'

---
+++ resources/lang/en-custom/admin/suppliers/message.php
@@ -0,0 +1,28 @@
+<?php
+
+return array(
+
+    'deleted' => 'Deleted supplier',
+    'does_not_exist' => 'Supplier does not exist.',
+
+
+    'create' => array(
+        'error'   => 'Supplier was not created, please try again.',
+        'success' => 'Supplier created successfully.'
+    ),
+
+    'update' => array(
+        'error'   => 'Supplier was not updated, please try again',
+        'success' => 'Supplier updated successfully.'
+    ),
+
+    'delete' => array(
+        'confirm'   => 'Are you sure you wish to delete this supplier?',
+        'error'   => 'There was an issue deleting the supplier. Please try again.',
+        'success' => 'Supplier was deleted successfully.',
+        'not_found'               => 'Supplier not found.',
+        'bulk_success'            => 'Suppliers were deleted successfully.',
+        'partial_success'        => 'Supplier deleted successfully. See additional information below. | :count suppliers were deleted successfully. See additional information below.',
+    )
+
+);
